acacb4d409e59751f528808ec27f96785d98975f
Glonel Dimapilis <gdimapilis@aol.com>
2023-01-26 18:00:46 -0600

    still trying to figure out last question

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -127,8 +127,8 @@ function find( value, array ){
   for (let i=0; i < array.length; i++) {
     if (value === array[i]) {
       return true;
-    } 
-  }
+    }
+  } 
 }
 console.log(find('d8', array))
 
